ac83b14e9804c9f0edfac7976a9561cda41d4c11
anderson92zolis <azolis92@hotmail.es>
2023-02-02 19:19:35 +0100

    revisiones hechas por Marina, Se implementa las correciones

--- src/n1exercici5/Main.java
+++ src/n1exercici5/Main.java
@@ -15,24 +15,27 @@ public class Main {
 	 * Ara el programa ha de serialitzar un Objecte Java a un fitxer .ser i després l’ha de desserialitzar.
 	 */
 	
+	ObjectOutputStream escribiendo_Fichero=null;
+	ObjectInputStream recuperando_Fichero= null;	
+		
 	Ordinador[] listaOrdenadores = new Ordinador[2];
 	
 	Ordinador ordenador1= new Ordinador();
-	Ordinador ordenador2= new Ordinador("MicrosoftSurface","500 gb");
+	Ordinador ordenador2= new Ordinador("MicrosoftSurface","500 gb");	
 	listaOrdenadores[0]= ordenador1;
 	listaOrdenadores[1]= ordenador2;
 	
 	try {
-		//salida
-		ObjectOutputStream escribiendo_Fichero= new ObjectOutputStream(new FileOutputStream("C:\\Users\\azoli\\eclipse-workspace\\Tasca-S1.05-Java-Utils\\src\\n1exercici5\\ordenador.ser"));
+		//output
+		escribiendo_Fichero= new ObjectOutputStream(new FileOutputStream("C:\\Users\\azoli\\eclipse-workspace\\Tasca-S1.05-Java-Utils\\src\\n1exercici5\\ordenador.ser"));
 		escribiendo_Fichero.writeObject(listaOrdenadores); 		
-		escribiendo_Fichero.close();
+	
 		  
-		//entrada
+		//input
 		
-		ObjectInputStream recuperando_Fichero= new ObjectInputStream(new FileInputStream("C:\\Users\\azoli\\eclipse-workspace\\Tasca-S1.05-Java-Utils\\src\\n1exercici5\\ordenador.ser"));
+		recuperando_Fichero= new ObjectInputStream(new FileInputStream("C:\\Users\\azoli\\eclipse-workspace\\Tasca-S1.05-Java-Utils\\src\\n1exercici5\\ordenador.ser"));
 		Ordinador[] ordenadoresRecuperadosEnArray = (Ordinador[]) recuperando_Fichero.readObject();
-		recuperando_Fichero.close();
+
 		
 		for (Ordinador ordenadores:ordenadoresRecuperadosEnArray) {
 			System.out.println("estos son los ordenadores guardados en .ser: " + ordenadores);
@@ -44,7 +47,13 @@ public class Main {
 		} catch (ClassNotFoundException ex) {
 			// TODO Auto-generated catch block
 			ex.printStackTrace();
-		}	
+		}	finally {
+				try {
+					if(escribiendo_Fichero!=null) {escribiendo_Fichero.close();}
+					if (recuperando_Fichero!=null) {recuperando_Fichero.close();}
+				} catch (Exception ex) {
+					ex.printStackTrace();
+					}
+			}
 	}
-
 }
